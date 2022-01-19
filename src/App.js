2ded10c525f2373a54b409ad699c2ad2246a83e7
Mladen Savic <mladenqdev@gmail.com>
2022-01-19 20:33:59 +0100

    added images, created basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+	{ src: '/img/matrix.jpg' },
+	{ src: '/img/25th-hour.jpg' },
+	{ src: '/img/dragon-tattoo.jpg' },
+	{ src: '/img/fight-club.jpg' },
+	{ src: '/img/in-bruges.jpg' },
+	{ src: '/img/lord-of-the-rings.jpg' },
+];
+
 function App() {
+	const [cards, setCards] = useState([]);
+	const [turns, setTurns] = useState(0);
+
+	// shuffle cards
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
+
+		setCards(shuffledCards);
+		setTurns(0);
+	};
+
+	console.log(cards, turns);
+
 	return (
 		<div className="App">
 			<h1>Movie Memory</h1>
-			<button>New Game</button>
+			<button onClick={shuffleCards}>New Game</button>
+
+			<div className="card-grid">
+				{cards.map((card) => (
+					<div className="card" key={card.id}>
+						<div>
+							<img className="front" src={card.src} alt="card front" />
+							<img className="back" src="/img/cover.jpg" alt="card back" />
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
